09d63583c730f260c88d539e2e6fbf48a04462b9
Pankil Kansal <pankilkansal3@gmail.com>
2024-12-14 19:34:08 -0500

    sectionone component

---
+++ src/Components/sectionone/sectionone.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function SectionOne() {
+  return (
+    <section className="section-1">
+      <h1 className="section-heading">Variety of Foods</h1>
+      <div className="section-1-icons"></div>
+    </section>
+  );
+}
+
+export default SectionOne;
