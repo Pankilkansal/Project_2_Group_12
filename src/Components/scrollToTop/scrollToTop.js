d8aabf032ad1549720bcbd2266118cf2b14c6393
Pankil Kansal <pankilkansal3@gmail.com>
2024-12-14 19:32:19 -0500

    scrollToTop component

---
+++ src/Components/scrollToTop/scrollToTop.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    isVisible && (
+      <button className="scroll-to-top show" onClick={scrollToTop}>
+        ↑ Scroll to Top
+      </button>
+    )
+  );
+}
+
+export default ScrollToTop;
